eb73f188cf6d823e1b8105e5214078ff50329b61
espenfana <espenfana@gmail.com>
2024-10-06 21:45:09 +0200

    improve testing

--- testing/testing_script.py
+++ testing/testing_script.py
@@ -4,6 +4,7 @@ from ectools.helper_functions import filename_parser
 
 import sys
 import os
+import random
 
 # Add the parent directory to sys.path to allow relational import of ectools
 sys.path.append(os.path.join(os.getcwd(), '..'))
@@ -16,13 +17,31 @@ ec.set_config('plotter', ec.Plotter.BOKEH)
 FPATH = 'data/'
 FNAME = '240912_01_MCL16_CSWWE1_LSV100-INIT_CO2_750C.DTA'
 
+# Importing and plotting single file
+try:
+    imp = ec.EcImporter(fname_parser=filename_parser)
+    f = imp.load_file(FPATH, FNAME)
+    f.plot()
+except Exception as e:
+    print('Single file import failed')
+    raise e
 
-imp = ec.EcImporter(fname_parser=filename_parser)
-f = imp.load_file(FPATH, FNAME)
+# Testing methods
+try:
+    imp = ec.EcImporter(fname_parser=filename_parser)
+    f = imp.load_file(FPATH, FNAME)
+    f.set_area(2)
+    r = random.randint(0, len(f.curr))
+    assert f.curr[r] == 2 * f.curr_dens[r], f'Current density not correct, curr:{f.curr[r]}, curr_dens:{f.curr_dens[r]}'
+except Exception as e:
+    print('Methods testing failed')
+    raise e
 
-f.plot_bokeh()
-
-f.set_area(0.489)
 #f.plot(y='curr_dens',hue='cycle', cycles=[1,2])
 
-fl = imp.load_folder(FPATH)
+# Loading a folder and returning an EcList
+try:
+    fl = imp.load_folder(FPATH)
+except Exception as e:
+    print('Folder loading failed')
+    raise e
